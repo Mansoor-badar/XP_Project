a421b086a0c294b836fb7936fa4ad19c81a41d7f
James <jamesellard@live.co.uk>
2026-03-05 10:09:51 +0000

    Initialised Card, Action and Icon reusable components

---
+++ frontend/src/components/entity/UploadCard.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import { Card } from "../../UI/Card.jsx";
+
+export default function UploadCard(props) {
+  // Initialisation ------------------------------
+  // State ---------------------------------------
+  // Handlers ------------------------------------
+  // View ----------------------------------------
+  return (
+    <Card>
+      
+    </Card>
+  );
+}
--- frontend/src/components/views/Home.jsx
+++ frontend/src/components/views/Home.jsx
@@ -3,9 +3,19 @@ import Layout from "../layout/Layout.jsx";
 function Home() {
   return (
     <Layout>
+      
+      
       <h1>Home</h1>
+    
+
+
+
     </Layout>
   );
 }
-
+//to do:
+// home -> upload/paste card
+//upload source file/write code input field
+//potentially supported language box (from wireframe)
+// run scan tab, with button/clear option
 export default Home;
